ad17c7317048868e52703987c7ff9d05a6bb360d
Naum Kostovski <kostovski.naum@gmail.com>
2011-11-23 16:44:18 +0100

    Added more fields to users, customized devise views for new and edit user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
+  ### Validations
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :username
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :first_name, :last_name, :username
 end
