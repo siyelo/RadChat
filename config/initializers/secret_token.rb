6c23aa636b7bcba7ee04c688070b48a43cebaf2c
Naum Kostovski <kostovski.naum@gmail.com>
2011-11-23 14:55:40 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radchat::Application.config.secret_token = '2464abfc2d57a6d6e1787749d9e23e44931bf6f53e2f61c5d6a63f1607861abaae0b0051e027e298ee89fbf2e4836f2e40e32c5a46ee0d202200a88dbb7db86d'
