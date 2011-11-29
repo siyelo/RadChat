0dc9f620abc722062bdb56b79f7f39df9123d5f4
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-11-29 16:30:26 +0100

    Improved styles for devise pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
-  def index
-  end
 
+  def index
+    if current_user
+      redirect_to company_url(current_user.current_company)
+    else
+      redirect_to new_user_session_url
+    end
+  end
 end
