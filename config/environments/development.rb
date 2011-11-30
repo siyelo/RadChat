af020ee306e12093685cf40496b946521881e0e8
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-11-30 10:12:02 +0100

    Remove mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,19 +31,10 @@ Radchat::Application.configure do
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   # A dummy setup for development - no deliveries, but logged
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :file
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   Paperclip.options[:command_path] = "/usr/local/bin/"
 end
 
-ActionMailer::Base.smtp_settings = {
-  :address => "smtp.gmail.com",
-  :port => 587,
-  :domain => "siyelo.com",
-  :authentication => :plain,
-  :user_name => 'mailer@siyelo.com',
-  :password => 'si@yelopassword',
-  :enable_starttls_auto => true
-}
